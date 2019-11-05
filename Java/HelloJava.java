1d32c49a758bec955f6263450ef2a9279049e0dd
terkiss <dl2000dl@naver.com>
2019-11-05 15:27:37 +0900

    initailized

---
+++ Java/HelloJava.java
@@ -0,0 +1,9 @@
+public class HelloJava
+{
+	public static void main(String[] args)
+	{
+		System.out.println("!!?!?!?!?!?!?!?!?!?!?!?!?!?!??!!?!?!?!?!? ? Java ~~~~");
+		System.out.println("gogogogogoogogogogo");
+		
+	}
+}
