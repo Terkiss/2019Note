e55e066b1501fa87f847adb10cc22a1762815528
terkiss <dl2000dl@naver.com>
2019-11-06 11:00:34 +0900

    2교시 끝

--- Java/HelloJava.java
+++
@@ -1,9 +0,0 @@
-public class HelloJava
-{
-	public static void main(String[] args)
-	{
-		System.out.println("!!?!?!?!?!?!?!?!?!?!?!?!?!?!??!!?!?!?!?!? ? Java ~~~~");
-		System.out.println("gogogogogoogogogogo");
-		
-	}
-}
