bd61c3cd2e694b9d9a04f2ee8815a2e7879a246d
terkiss <dl2000dl@naver.com>
2019-11-07 12:59:05 +0900

    2019 11 07 오전

---
+++ AndroidProjects/app/src/main/java/com/Dotge/MyApp/MainActivity.java
@@ -0,0 +1,14 @@
+package com.Dotge.MyApp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
