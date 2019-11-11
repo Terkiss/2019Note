1ba395e35c64aea4007379b88a3f14dceb0050b3
terkiss <dl2000dl@naver.com>
2019-11-11 12:46:14 +0900

    2019 11 11 오전 수업 완료

--- JavaProjects/Chap01/src/systemout/SystenIytOrintln.java
+++ JavaProjects/Chap01/src/systemout/SystenIytOrintln.java
@@ -1,9 +1,9 @@
 package systemout;
 
-import javax.swing.*;
+
 
 public class SystenIytOrintln {
-
+	static int a;
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println(7);
@@ -13,5 +13,29 @@ public class SystenIytOrintln {
 		System.out.println("3+5"+"의 연산 결과는 8입니다.");
 		System.out.println(3+5);	
 	    
+		int num1 = 10 ;
+		int num2 = 20 ;
+		
+		int num3 = num1 + num2 ;
+		
+		System.out.println(num1 + "+" + num2 + "=" + num3) ;
+		
+		int decimal = 12;
+		int hexdecimal = 0xC;
+		int octet = 014;
+		int binary = 0b1100;
+		
+		System.out.println(decimal);
+		System.out.println(hexdecimal);
+		System.out.println(octet);
+		System.out.println(binary);
+		
+		char firsrchar =  'A' ;
+		char firstchar2 = '\u0041' ;
+		char ch = 'a' ;
+		char ch2 = '한' ;
+		
+		System.out.println(firsrchar +" "+ firstchar2  + " " + ch +" "+ ch2 ) ;
+		
 	}
 }
