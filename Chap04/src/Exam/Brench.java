47ace8eb567495efb7b09dee48e665276d5b11d2
terkiss <dl2000dl@naver.com>
2019-11-15 14:35:27 +0900

    윈도우 자동화 프로그램 등록 by terkiss

---
+++ Chap04/src/Exam/Brench.java
@@ -0,0 +1,18 @@
+package Exam;
+
+public class Brench {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a = 20 ;
+		int aa = 20 ;
+		int aaa = 30 ;
+		
+		if(a < aaa)
+		{
+			System.out.println("aa 가 작당") ;
+			System.out.println("aaa가 크당") ;
+		}
+	}
+
+}
