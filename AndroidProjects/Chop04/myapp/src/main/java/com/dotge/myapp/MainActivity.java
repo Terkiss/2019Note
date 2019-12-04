33a98090ac8996c8426613d6b29e5e99c6591595
유정훈 <dl2000dl@naver.com>
2019-12-05 00:57:38 +0900

    오늘자 커밋

--- AndroidProjects/Chop04/myapp/src/main/java/com/dotge/myapp/MainActivity.java
+++ AndroidProjects/Chop04/myapp/src/main/java/com/dotge/myapp/MainActivity.java
@@ -1,12 +1,12 @@
 package com.dotge.myapp;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -21,18 +21,146 @@ public class MainActivity extends AppCompatActivity {
 
         //findViewBuId
         // R.id
-        final EditText inputNum1 = (EditText) findViewById(R.id.txtnum);
-        final EditText inputNum2 = (EditText) findViewById(R.id.txtnum2);
-        final TextView result = (TextView) findViewById(R.id.resultcal);
-        Button add = (Button) findViewById(R.id.addbtn);
+
+
+        final Button operatorView, plusBtn, minusBtn, muxBtn, diviBtn;
+
+
+
+        final EditText inputNum1        =       (EditText) findViewById(R.id.txtnum)    ;
+        final EditText inputNum2        =       (EditText) findViewById(R.id.txtnum2)   ;
+        final TextView result           =       (TextView) findViewById(R.id.resultcal) ;
+
+        Button add      =       (Button) findViewById(R.id.calulatebtn)     ;
+        operatorView    =       (Button) findViewById(R.id.operaterView)    ;
+        plusBtn         =       (Button) findViewById(R.id.plusbtn)         ;
+        minusBtn        =       (Button) findViewById(R.id.minusbtn)        ;
+        muxBtn          =       (Button) findViewById(R.id.multibtn)        ;
+        diviBtn         =       (Button) findViewById(R.id.divisionbtn)     ;
+
+
+        operatorView.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v)
+            {
+                operatorView.setVisibility(View.INVISIBLE);
+                plusBtn.setVisibility(View.VISIBLE);
+                minusBtn.setVisibility(View.VISIBLE);
+                muxBtn.setVisibility(View.VISIBLE);
+                diviBtn.setVisibility(View.VISIBLE);
+            }
+        });
+
+        plusBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                minusBtn.setVisibility(View.INVISIBLE);
+                muxBtn.setVisibility(View.INVISIBLE);
+                diviBtn.setVisibility(View.INVISIBLE);
+            }
+        });
+        minusBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                plusBtn.setVisibility(View.INVISIBLE);
+                muxBtn.setVisibility(View.INVISIBLE);
+                diviBtn.setVisibility(View.INVISIBLE);
+            }
+        });
+        muxBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                plusBtn.setVisibility(View.INVISIBLE);
+                minusBtn.setVisibility(View.INVISIBLE);
+                diviBtn.setVisibility(View.INVISIBLE);
+            }
+        });
+        diviBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                plusBtn.setVisibility(View.INVISIBLE);
+                minusBtn.setVisibility(View.INVISIBLE);
+                muxBtn.setVisibility(View.INVISIBLE);
+
+            }
+        });
+
 
         add.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                int number1 = convert2Int( inputNum1.getText() );
-                int number2 = convert2Int( inputNum2.getText() );
-                int sum = number1 + number2;
-                result.setText(String.valueOf(sum));
+             //   int number1 = convert2Int( inputNum1.getText() );
+                //int number2 = convert2Int( inputNum2.getText() );
+                //int sum = number1 + number2;
+
+                if(operatorView.getVisibility() == View.VISIBLE)
+                {
+                    result.setText("연산자를 골르고 버튼을 눌러 바보야");
+                }
+                else
+                {
+                    if(inputNum1.getText().toString().length() < 1 || inputNum2.getText().toString().length() < 1)
+                    {
+                        result.setText(String.valueOf("값을 넣어야지..."));
+
+                    }
+                    else if(plusBtn.getVisibility() == View.VISIBLE)
+                    {
+                        int number1 = convert2Int( inputNum1.getText() );
+                        int number2 = convert2Int( inputNum2.getText() );
+                        int sum = number1 + number2;
+                        result.setText(String.valueOf(sum));
+
+                        operatorView.setVisibility(View.VISIBLE);
+                        plusBtn.setVisibility(View.INVISIBLE);
+                        minusBtn.setVisibility(View.INVISIBLE);
+                        muxBtn.setVisibility(View.INVISIBLE);
+                        diviBtn.setVisibility(View.INVISIBLE);
+                    }
+                    else if(minusBtn.getVisibility() == View.VISIBLE)
+                    {
+                        int number1 = convert2Int( inputNum1.getText() );
+                        int number2 = convert2Int( inputNum2.getText() );
+                        int sum = number1 - number2;
+                        result.setText(String.valueOf(sum));
+
+                        operatorView.setVisibility(View.VISIBLE);
+                        plusBtn.setVisibility(View.INVISIBLE);
+                        minusBtn.setVisibility(View.INVISIBLE);
+                        muxBtn.setVisibility(View.INVISIBLE);
+                        diviBtn.setVisibility(View.INVISIBLE);
+                    }
+                    else if(muxBtn.getVisibility() == View.VISIBLE)
+                    {
+                        int number1 = convert2Int( inputNum1.getText() );
+                        int number2 = convert2Int( inputNum2.getText() );
+                        int sum = number1 * number2;
+                        result.setText(String.valueOf(sum));
+
+                        operatorView.setVisibility(View.VISIBLE);
+                        plusBtn.setVisibility(View.INVISIBLE);
+                        minusBtn.setVisibility(View.INVISIBLE);
+                        muxBtn.setVisibility(View.INVISIBLE);
+                        diviBtn.setVisibility(View.INVISIBLE);
+                    }
+                    else if(diviBtn.getVisibility() == View.VISIBLE)
+                    {
+                        int number1 = convert2Int( inputNum1.getText() );
+                        int number2 = convert2Int( inputNum2.getText() );
+                        int sum = number1 / number2;
+                        result.setText(String.valueOf(sum));
+
+                        operatorView.setVisibility(View.VISIBLE);
+                        plusBtn.setVisibility(View.INVISIBLE);
+                        minusBtn.setVisibility(View.INVISIBLE);
+                        muxBtn.setVisibility(View.INVISIBLE);
+                        diviBtn.setVisibility(View.INVISIBLE);
+                    }
+                }
+
+
+                //result.setText(String.valueOf(sum));
             }
         });
 
--- AndroidProjects/Chop04/selfcheck2/src/main/java/com/dotge/selfcheck2/MainActivity.java
+++ AndroidProjects/Chop04/selfcheck2/src/main/java/com/dotge/selfcheck2/MainActivity.java
@@ -1,17 +1,19 @@
 package com.dotge.selfcheck2;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
-import android.util.EventLog;
 import android.view.MotionEvent;
 import android.view.View;
 import android.widget.Button;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
+import androidx.appcompat.app.AppCompatActivity;
+
 public class MainActivity extends AppCompatActivity {
 
+    int Tchcount = 0;
+    TextView xposition, yposition;
+    TextView touchCount;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -19,9 +21,7 @@ public class MainActivity extends AppCompatActivity {
 
 
         LinearLayout ll ;
-        int Tchcount = 0;
-        final TextView xposition, yposition;
-        TextView touchCount;
+
         Button exitButton;
 
         ll = (LinearLayout) findViewById(R.id.linearLayout);
@@ -41,7 +41,9 @@ public class MainActivity extends AppCompatActivity {
                 xposition.setText("X좌표 :: "+pos);
                 pos = String.valueOf(event.getY());
                 yposition.setText("Y좌표 :: "+pos);
-                pos = "터치한 횟수 :: ";
+                pos = "터치한 횟수 :: "+(++Tchcount);
+                touchCount.setText(pos);
+
 
 
 
