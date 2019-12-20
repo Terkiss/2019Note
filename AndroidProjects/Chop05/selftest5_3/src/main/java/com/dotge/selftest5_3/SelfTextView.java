71976943abaa456d6ad4f167baddaf9094b5512b
terkiss <dl2000dl@naver.com>
2019-12-20 15:19:00 +0900

    20191220 -2

---
+++ AndroidProjects/Chop05/selftest5_3/src/main/java/com/dotge/selftest5_3/SelfTextView.java
@@ -0,0 +1,24 @@
+package com.dotge.selftest5_3;
+
+import android.widget.TextView;
+
+public class SelfTextView {
+    private String Id;
+    private TextView textView;
+
+    public String getId() {
+        return Id;
+    }
+
+    public void setId(String id) {
+        Id = id;
+    }
+
+    public TextView getTextView() {
+        return textView;
+    }
+
+    public void setTextView(TextView textView) {
+        this.textView = textView;
+    }
+}
