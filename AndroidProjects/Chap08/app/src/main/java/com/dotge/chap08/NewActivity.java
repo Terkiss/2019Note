98c6a6f909d6788f6739c05e561fdb2403984c95
terkiss <dl2000dl@naver.com>
2020-02-04 12:55:42 +0900

    commit

---
+++ AndroidProjects/Chap08/app/src/main/java/com/dotge/chap08/NewActivity.java
@@ -0,0 +1,41 @@
+package com.dotge.chap08;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class NewActivity extends AppCompatActivity {
+    Intent intent;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_new);
+
+        Button button = findViewById(R.id.button);
+
+
+        intent = getIntent();
+        String name = intent.getStringExtra("activity");
+
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                intent = new Intent();
+                int sum=0;
+                for(int i = 0 ; i <=100 ; i++)
+                {
+                    sum+=i;
+                }
+                intent.putExtra("sum", sum);
+                intent.putExtra("name", "NewActivity");
+                setResult(RESULT_OK, intent);
+                finish();
+            }
+        });
+    }
+}
