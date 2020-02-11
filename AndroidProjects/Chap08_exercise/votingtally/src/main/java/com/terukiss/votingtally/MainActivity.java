185687be0725cf89aca7267cedffe5083690cf99
유정훈 <dl2000dl@naver.com>
2020-02-12 02:09:13 +0900

    aaaa

---
+++ AndroidProjects/Chap08_exercise/votingtally/src/main/java/com/terukiss/votingtally/MainActivity.java
@@ -0,0 +1,53 @@
+package com.terukiss.votingtally;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.media.Image;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+    int voteRating[];
+    String name[]= {"1번", "2번", "3번", "4번", "5번", "6번", "7번", "8번", "9번"};
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        int imgFiled[] = new int[9];
+        ImageView imageView[] = new ImageView[9];
+        voteRating = new int[9];
+        for (int i = 0; i < imgFiled.length; i++) {
+            //      이름                      타입                  패키지명
+            imgFiled[i] = getResources().getIdentifier("@drawable/pic" + (i + 1), "drawable", this.getPackageName());
+            int id = getResources().getIdentifier("pic" + (i + 1), "id", this.getPackageName());
+            imageView[i] = findViewById(id);
+            Log.d("Voting", "이미지 동적 아이디" + imgFiled[i] + " 이미지뷰 동적 아이디  " + id + "  " + imageView[i]);
+        }
+        for (int i = 0; i < imgFiled.length; i++) {
+            imageView[i].setImageResource(imgFiled[i]);
+            voteRating[i] = 0;
+        }
+
+        for (int i = 0; i < imageView.length; i++)
+        {
+            final int index = i;
+            imageView[index].setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    voteRating[index]++;
+                    Toast.makeText(getApplicationContext(), name[index]+" : 총" +voteRating[index]+"표",Toast.LENGTH_LONG).show();
+                }
+            });
+        }
+
+
+    }
+
+
+
+
+}
