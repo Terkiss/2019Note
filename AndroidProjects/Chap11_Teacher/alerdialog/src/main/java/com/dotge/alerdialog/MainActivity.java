0d8987754ed56a2643a242eb6fe3c2098bd36bca
terkiss <dl2000dl@naver.com>
2020-02-28 15:34:10 +0900

    aa

---
+++ AndroidProjects/Chap11_Teacher/alerdialog/src/main/java/com/dotge/alerdialog/MainActivity.java
@@ -0,0 +1,123 @@
+package com.dotge.alerdialog;
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.DialogInterface;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+
+    EditText edt_Name, edt_Email, dig_Name, dig_Email;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        edt_Name = findViewById(R.id.editText);
+        edt_Email = findViewById(R.id.editText2);
+
+        Button button = findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
+                View layout = View.inflate(MainActivity.this, R.layout.dialog,null);
+                builder.setView(layout);
+                builder.setTitle("사용자 정보 입력");
+                builder.setIcon(R.drawable.ic_menu_allfriends);
+                dig_Name = layout.findViewById(R.id.editText3);
+                dig_Email = layout.findViewById(R.id.editText4);
+                dig_Name.setText(edt_Name.getText().toString());
+                dig_Email.setText(edt_Email.getText().toString());
+
+                builder.setPositiveButton("확인", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        edt_Name.setText(dig_Name.getText().toString());
+                        edt_Email.setText(dig_Email.getText().toString());
+                    }
+                });
+
+                builder.setNegativeButton("취소", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        Toast.makeText(getApplicationContext(),"취소했습니다",Toast.LENGTH_LONG).show();
+                    }
+                });
+
+                builder.show();
+            }
+        });
+    }
+
+ /*
+    public void a()
+    {
+        final String[] singer = {"AOA", "트와이스", "APINK", "여자친구"};
+        final ArrayList<String> item = new ArrayList<String>();
+
+        AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
+
+        builder.setTitle("좋아하는 걸그룹은?");
+        builder.setMultiChoiceItems(singer, null, new DialogInterface.OnMultiChoiceClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which, boolean isChecked) {
+                String value = singer[which];
+                if(isCheck)
+                {
+                    item.add(value);
+                }
+                else if (item.contains(value))
+                {
+                    item.remove(value);
+                }
+            }
+        });
+        builder.setPositiveButton("확인", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                String str = "";
+                for(int i = 0 ; i< item.size(); i++)
+                {
+                    str += "\n" + item.get(i);
+                }
+                Toast.makeText(getApplicationContext(), "aaa", Toast.LENGTH_LONG).show();
+            }
+        });
+
+        builder.show();
+
+
+
+    }
+*/
+
+    @Override
+    public void onBackPressed() {
+        AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
+        builder.setTitle("종료 확인");
+        builder.setMessage("현재 프로그램을 종료하시겠습니까?");
+        builder.setPositiveButton("확인", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                finish();
+            }
+        });
+
+        builder.setNegativeButton("취소", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                Toast.makeText(getApplicationContext(),"취소했습니다",Toast.LENGTH_LONG).show();
+            }
+        });
+
+        builder.show();
+    }
+}
