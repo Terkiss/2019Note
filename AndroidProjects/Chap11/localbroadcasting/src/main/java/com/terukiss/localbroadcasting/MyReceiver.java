a26896d57db601aeabad31f19034c752aa5a59f6
Terkiss <dl2000dl@naver.com>
2020-03-05 02:04:02 +0900

    aaa

--- AndroidProjects/Chap11/localbroadcasting/src/main/java/com/terukiss/localbroadcasting/MyReceiver.java
+++ AndroidProjects/Chap11/localbroadcasting/src/main/java/com/terukiss/localbroadcasting/MyReceiver.java
@@ -7,13 +7,19 @@ import android.util.Log;
 import android.widget.Toast;
 
 public class MyReceiver extends BroadcastReceiver {
-    public static final String My_ACTION = "action.ACTION_MY_REACT";
+    public static final String My_ACTION = "com.My.Broad.Receive";
 
     @Override
     public void onReceive(Context context, Intent intent) {
+        // 브로드 캐스트도 인텐트가 옴으로 인텐트를 통한 데이터 전송이 가능해 보인다 일단 해보자
         if (My_ACTION.equals(intent.getAction())){
             Toast.makeText(context,"나만의 방송이 수신되었습니다.",Toast.LENGTH_LONG).show();
+            String a;
+            a = intent.getStringExtra("key")+"\n";
+            a +=intent.getStringExtra("key1")+"\n";
+            a +=intent.getStringExtra("key2")+"\n";
             Log.d("Jeong", "나의 방송 수신 됨 ");
+            Log.d("Jeong", " \n"+a);
             abortBroadcast();
         }
     }
--- AndroidProjects/Chap11/systembreoadcasting/src/main/java/com/terukiss/systembreoadcasting/MainActivity.java
+++ AndroidProjects/Chap11/systembreoadcasting/src/main/java/com/terukiss/systembreoadcasting/MainActivity.java
@@ -1,4 +1,4 @@
-package com.cyberkyj.system_broadcastreceiver;
+package com.terukiss.systembreoadcasting;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
@@ -12,17 +12,19 @@ import android.content.IntentFilter;
 import android.content.pm.PackageManager;
 import android.os.BatteryManager;
 import android.os.Bundle;
-import android.widget.Adapter;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.Toast;
 
 import java.util.ArrayList;
 
+
 public class MainActivity extends AppCompatActivity {
 
     ListView listView;
+
     ArrayList<String> datas;
+
     ArrayAdapter<String> adapter;
 
     @Override
@@ -30,15 +32,24 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        listView = findViewById(R.id.list); // 리스트뷰
+        listView = findViewById(R.id.list);
         datas = new ArrayList<>();
-        adapter = new ArrayAdapter<String>(this,android.R.layout.simple_list_item_1,datas);  // 어답터 생성
-        listView.setAdapter(adapter); // 어답터 지정
+        adapter = new ArrayAdapter<String>(this,android.R.layout.simple_list_item_1,datas);
+        listView.setAdapter(adapter);
 
+        // 배터리 상황을 파악
+        // 실제 브로드 캐스트 리시버를 등록하는 구문이 아니라
+        // 시스템 배터리 상태의 정보값만 얻기 위한구문
         IntentFilter filter = new IntentFilter(Intent.ACTION_BATTERY_CHANGED);
+                                    // 여기에 따로 리시버를 넣는다면? 알아보자
         Intent batteryStatus = registerReceiver(null,filter);
+
+
+        // 스마트 폰에 배터리가 차징 되고 있니 ? ? ?
         int status = batteryStatus.getIntExtra(BatteryManager.EXTRA_STATUS, -1);
         boolean isCharging = status==BatteryManager.BATTERY_STATUS_CHARGING;
+
+        // 배터리가 차징 되고 있다면
         if(isCharging){
             int chargePlug = batteryStatus.getIntExtra(BatteryManager.EXTRA_PLUGGED, -1);
             boolean usbCharge =  chargePlug == BatteryManager.BATTERY_PLUGGED_USB;
@@ -52,21 +63,23 @@ public class MainActivity extends AppCompatActivity {
             addListItem("Battery State is not Charging");
         }
 
+
         int level = batteryStatus.getIntExtra(BatteryManager.EXTRA_LEVEL,-1);
         int scale = batteryStatus.getIntExtra(BatteryManager.EXTRA_SCALE, -1);
         float pct = (level/(float)scale)*100;
         addListItem("Current Battery : "+pct+"%");
-
+        // 브로드 캐스트 등록
         registerReceiver(brOn,new IntentFilter(Intent.ACTION_SCREEN_ON));
         registerReceiver(brOff,new IntentFilter(Intent.ACTION_SCREEN_OFF));
         registerReceiver(batteryReceiver, new IntentFilter(Intent.ACTION_POWER_CONNECTED));
         registerReceiver(batteryReceiver, new IntentFilter(Intent.ACTION_POWER_DISCONNECTED));
 
-        if(ContextCompat.checkSelfPermission(this, Manifest.permission.PROCESS_OUTGOING_CALLS) != PackageManager.PERMISSION_GRANTED ||
-           ContextCompat.checkSelfPermission(this,Manifest.permission.READ_PHONE_STATE) != PackageManager.PERMISSION_GRANTED ||
-           ContextCompat.checkSelfPermission(this,Manifest.permission.READ_CALL_LOG) != PackageManager.PERMISSION_GRANTED){
-           ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.PROCESS_OUTGOING_CALLS, Manifest.permission.READ_PHONE_STATE, Manifest.permission.READ_CALL_LOG}, 100);
 
+        // 위험 권한 설정
+        if( ContextCompat.checkSelfPermission(this, Manifest.permission.PROCESS_OUTGOING_CALLS) != PackageManager.PERMISSION_GRANTED || ContextCompat.checkSelfPermission(this,Manifest.permission.READ_PHONE_STATE) != PackageManager.PERMISSION_GRANTED || ContextCompat.checkSelfPermission(this,Manifest.permission.READ_CALL_LOG) != PackageManager.PERMISSION_GRANTED){
+
+            // 위험 권한 요청
+            ActivityCompat.requestPermissions(this, new String[]{ Manifest.permission.PROCESS_OUTGOING_CALLS, Manifest.permission.READ_PHONE_STATE, Manifest.permission.READ_CALL_LOG }, 100);
         }
     }
 
@@ -74,6 +87,7 @@ public class MainActivity extends AppCompatActivity {
     public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
         super.onRequestPermissionsResult(requestCode, permissions, grantResults);
         if (requestCode == 100 && grantResults.length > 0) {
+            // 하나라도 권한이 승이이 안되었다면 .
             if (grantResults[0] != PackageManager.PERMISSION_GRANTED || grantResults[1] != PackageManager.PERMISSION_GRANTED || grantResults[2] != PackageManager.PERMISSION_GRANTED) {
                 Toast toast = Toast.makeText(this, "no permission", Toast.LENGTH_SHORT);
                 toast.show();
@@ -94,6 +108,8 @@ public class MainActivity extends AppCompatActivity {
         adapter.notifyDataSetChanged();
     }
 
+    // 화면 on/ off 이상황을 체크해서 특정 로직의 수행을 구동하거나 정지
+    // 액티비티 서비스 들의 코드 에서 동적으로 등록해야만 실행
     BroadcastReceiver brOn = new BroadcastReceiver() {
         @Override
         public void onReceive(Context context, Intent intent) {
